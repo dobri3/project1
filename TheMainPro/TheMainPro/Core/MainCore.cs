588019588d0bde0aff6746ebed03f0f3d921e9be
dobri3 <142723822+dobri3@users.noreply.github.com>
2024-04-07 21:25:51 +0300

    Add files via upload

---
+++ TheMainPro/TheMainPro/Core/MainCore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace TheMainPro.Core
+{
+    internal class MainCore
+    {
+        public static Frame MainFrameCore { get; set; }
+    }
+}
